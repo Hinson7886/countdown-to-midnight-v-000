c561281cd032f9b69e1c1532c42fc11164223396
Jon Hinson <jonhinsonfx@gmail.com>
2017-12-01 18:44:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown
-    counter = 10
+    counter = 5
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
   end
-
+  return "HAPPY NEW YEAR!"
 end
-return "HAPPY NEW YEAR!"
