fc14bfc0d8c5c18028661907b0a7f1a2e2afb042
Jon Hinson <jonhinsonfx@gmail.com>
2017-12-01 19:43:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,5 +11,11 @@ end
 
 
 def countdown_with_sleep(n)
-
+  counter = n
+  while counter > 0
+    sleep(1)
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
